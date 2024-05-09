f51de808837df7751a9fc170f5071fcfe15791c8
Anna P <atnasvk@ya.ru>
2024-05-09 17:13:11 +0300

    registration without redirects

--- src/main/java/ru/minusd/security/config/SecurityConfiguration.java
+++ src/main/java/ru/minusd/security/config/SecurityConfiguration.java
@@ -45,8 +45,7 @@ public class SecurityConfiguration {
                 // Настройка доступа к конечным точкам
                 .authorizeHttpRequests(request -> request
                         // Можно указать конкретный путь, * - 1 уровень вложенности, ** - любое количество уровней вложенности
-                        .requestMatchers("/auth/**").permitAll()
-                        .requestMatchers("/swagger-ui/**", "/swagger-resources/*", "/v3/api-docs/**").permitAll()
+                        .requestMatchers("/register", "/login").permitAll()
                         .requestMatchers("/endpoint", "/admin/**").hasRole("ADMIN")
                         .anyRequest().authenticated())
                 .sessionManagement(manager -> manager.sessionCreationPolicy(STATELESS))
--- src/main/java/ru/minusd/security/controller/AuthController.java
+++ src/main/java/ru/minusd/security/controller/AuthController.java
@@ -4,31 +4,38 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 import ru.minusd.security.domain.dto.JwtAuthenticationResponse;
 import ru.minusd.security.domain.dto.SignInRequest;
 import ru.minusd.security.domain.dto.SignUpRequest;
 import ru.minusd.security.service.AuthenticationService;
 
-@RestController
-@RequestMapping("/auth")
+@Controller
 @RequiredArgsConstructor
 @Tag(name = "Аутентификация")
 public class AuthController {
     private final AuthenticationService authenticationService;
 
+    @GetMapping("/register")
+    public String getRegistrationForm() {
+        return "register";
+    }
+
     @Operation(summary = "Регистрация пользователя")
-    @PostMapping("/sign-up")
-    public JwtAuthenticationResponse signUp(@RequestBody @Valid SignUpRequest request) {
+    @PostMapping("/register")
+    public JwtAuthenticationResponse signUp(@ModelAttribute SignUpRequest request) {
         return authenticationService.signUp(request);
     }
 
+    @GetMapping("/login")
+    public String getLoginForm() {
+        return "login";
+    }
+
     @Operation(summary = "Авторизация пользователя")
-    @PostMapping("/sign-in")
-    public JwtAuthenticationResponse signIn(@RequestBody @Valid SignInRequest request) {
+    @PostMapping("/login")
+    public JwtAuthenticationResponse signIn(@ModelAttribute SignInRequest request) {
         return authenticationService.signIn(request);
     }
 }
